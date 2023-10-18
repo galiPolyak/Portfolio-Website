06260793755c2020fe7241290f22021d3a6f92d3
Gali Polyak <galipolyak@Galis-MacBook-Air.local>
2023-10-17 21:10:08 -0400

    Remove .DS_Store from current directory

--- galiApp.js
+++ galiApp.js
@@ -5,3 +5,27 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+  import { Application } from '@splinetool/runtime';
+
+  const canvas = document.getElementById('canvas3d');
+  const app = new Application(canvas);
+  app.load('https://prod.spline.design/1wdCF8KXGJ1JEVyH/scene.splinecode');

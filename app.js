7ca9b83c50792e22a581ce3e50c1c1d82d3dcf9b
Gali Polyak <galipolyak@Galis-MacBook-Air.local>
2023-11-16 02:47:44 -0500

    Updated

--- app.js
+++ app.js
@@ -22,10 +22,61 @@ function reveal() {
     }
   }
   
-  window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", reveal);
 
-  import { Application } from '@splinetool/runtime';
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-  const canvas = document.getElementById('canvas3d');
-  const app = new Application(canvas);
-  app.load('https://prod.spline.design/1wdCF8KXGJ1JEVyH/scene.splinecode');
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('navbar__logo');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".navbar__logo > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
---
+++ bubbles.js
@@ -0,0 +1,126 @@
+// SETUP:
+const imageSources = ['images/bubbleImg.png', 'images/bubbleImg2.png', 'images/bubbleImg3.png'];
+
+// Create an array to store the Image objects
+const bubbleImages = [];
+
+bubbleImages[0] = new Image();
+bubbleImages[1] = new Image();
+bubbleImages[2] = new Image();
+
+bubbleImages[0].src = 'images/bubbleImg.png';
+bubbleImages[1].src= 'images/bubbleImg2.png';
+bubbleImages[2].src= 'images/bubbleImg.png';
+
+
+/*const bubbleImg = new Image();
+const bubbleImg2 = new Image();
+
+bubbleImg.src = 'images/bubbleImg.png'; // Ensure the path to your bubble image is correct
+bubbleImg2.src= 'images/bubbleImg2.png';*/
+
+const canvas = document.getElementById('myCanvas');
+const c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - 80;
+
+let mouse = {
+    x: undefined,
+    y: undefined
+};
+
+// VARIABLES:
+
+let gravity = 0.8;
+let yFriction = 0.75;
+let xFriction = 0.75;
+
+// EVENT LISTENERS:
+
+window.addEventListener("mousemove", function(e) {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+});
+
+window.addEventListener("resize", function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 80;
+    // Uncomment the below line if you want to reinitialize the bubbles when the window is resized
+    // init();
+});
+
+// BUBBLE FUNCTIONS:
+
+function Bubble(x, y, velX, velY, radius) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.radius = radius;
+    this.bubbleImageIndex = randomIntInRange(0, bubbleImages.length - 1);
+
+    this.update = function() {
+
+        if (this.y + this.radius + this.velY > canvas.height) {
+            this.velY = -this.velY * yFriction;
+        } else {
+            this.velY += gravity;
+        }
+
+        if (this.x + this.radius + this.velX > canvas.width || 
+            this.x - this.radius + this.velX < 0) {
+            this.velX = -this.velX * xFriction;
+        }
+
+        // Hover over to bounce the bubbles
+        if (mouse.x > (this.x - this.radius) && mouse.x < (this.x + this.radius) && 
+            mouse.y > (this.y - this.radius) && mouse.y < (this.y + this.radius)) {
+            this.velX = randomIntInRange(-10, 10);
+            this.velY = -15;
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+
+        this.draw();
+    };
+
+    this.draw = function() {
+        c.drawImage(bubbleImages[this.bubbleImageIndex], this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
+    };
+}
+
+// HELPER FUNCTIONS:
+
+function randomIntInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// MAIN PROGRAM:
+
+var bubbleArray = [];
+
+function init() {
+    bubbleArray = [];
+    for (let i = 0; i < 100; i++) {
+        let radius = randomIntInRange(10, 80);
+        let x = randomIntInRange(radius, canvas.width - radius);
+        let y = randomIntInRange(0, canvas.height - radius);
+        let velX = randomIntInRange(-1, 1);
+        let velY = randomIntInRange(-1, 1);
+        bubbleArray.push(new Bubble(x, y, velX, velY, radius));
+    }   
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < bubbleArray.length; i++) {
+        bubbleArray[i].update();
+    }
+}
+
+init();
+animate();
